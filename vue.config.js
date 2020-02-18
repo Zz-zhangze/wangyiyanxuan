ffb780d91d25af5ef7509476408440ae7d327382
zhangze <987034903@qq.com>
2020-02-18 17:10:32 +0800

    san

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,19 @@
-lintOnSave: false
 const px2rem = require('postcss-px2rem')
 // 配置postcs-px2rem
 const postcss = px2rem({
-  remUnit: 75   //基准大小 baseSize，需要和rem.js中单位rem值占比一样相同
+  remUnit: 75 ,  //基准大小 baseSize，需要和rem.js中单位rem值占比一样相同
 })
-css: { // 添加postcss配置
-    loaderOptions: {
-      postcss: {
-        plugins: [
-          postcss
-        ]
+
+module.exports = {
+  lintOnSave: false,
+
+  css: { // 添加postcss配置
+      loaderOptions: {
+        postcss: {
+          plugins: [
+            postcss
+          ]
+        }
       }
-    }
+  } 
 }
